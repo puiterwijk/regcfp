26e7a8cecb6d8d8256d47e756e82cbc4f33d6df0
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-08-04 22:30:28 +0200

    ifGT* is no longer needed

--- app.js
+++ app.js
@@ -43,20 +43,6 @@ var hbs = handlebars.create({
       } else {
         return options.inverse(this);
       }
-    },
-    ifGTE: function(v1, v2, options) {
-      if(v1 >= v2) {
-        return options.fn(this);
-      } else {
-        return options.inverse(this);
-      }
-    },
-    ifGT: function(v1, v2, options) {
-      if(v1 > v2) {
-        return options.fn(this);
-      } else {
-        return options.inverse(this);
-      }
     }
   }
 });
