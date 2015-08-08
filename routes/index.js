720ce84ca0075ee4823d154097d13b320e53b54d
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-08-08 14:35:45 +0200

    viewname

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ var RegistrationPayment = models.RegistrationPayment;
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + '/../config/config.json')[env];
 
+var utils = require('../utils');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,6 +27,15 @@ router.get('/', function(req, res, next) {
   } else {
     res.render('index/index', { });
   }
+});
+
+router.all('/view_name', utils.require_permission('registration/view_name'));
+router.post('/view_name', function(req, res, next) {
+  var userid = req.body.userid;
+  User.findOne({where: {id:userid}, include: [Registration]})
+    .then(function(user) {
+      res.render('index/view_user', { user: user });
+    });
 });
 
 module.exports = router;
