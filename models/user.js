2cfa430865ba281a25452cd38ac79b8c03deb3e8
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-08-08 15:11:46 +0200

    View user

--- models/user.js
+++ models/user.js
@@ -3,7 +3,9 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     email: DataTypes.STRING,
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    isInelligbileForRaffle: DataTypes.BOOLEAN,
+    isVolunteer: DataTypes.BOOLEAN
   }, {
     classMethods: {
       associate: function(models) {
--- routes/index.js
+++ routes/index.js
@@ -30,11 +30,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.all('/view_name', utils.require_permission('registration/view_name'));
-router.post('/view_name', function(req, res, next) {
-  var userid = req.body.userid;
-  User.findOne({where: {id:userid}, include: [Registration]})
-    .then(function(user) {
-      res.render('index/view_user', { user: user });
+router.get('/view_name', function(req, res, next) {
+  var everyone = req.query.everyone;
+  User.findAll({include: [Registration]})
+    .then(function(users) {
+      var users2 = [];
+      for(var user in users) {
+        user = users[user];
+        if(user.Registration.badge_printed && !user.isInelligbileForRaffle && (everyone || user.isVolunteer)) {
+          users2.push(user);
+        }
+      };
+      var name = null;
+      if(req.query.index) {
+        name = users[name].name;
+      }
+      res.render('index/view_name', { count: users2.length, name: name });
     });
 });
 
