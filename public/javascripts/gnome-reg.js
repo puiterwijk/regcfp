c6efc6c6804068373fc61dea2a09b3500ec14837
Benjamin Berg <benjamin@sipsolutions.net>
2016-06-14 00:06:28 +0200

    gnome: registration improvements suggested by aday

--- public/javascripts/gnome-reg.js
+++ public/javascripts/gnome-reg.js
@@ -10,7 +10,6 @@ $(function() {
 			amount = 0;
 		}
 		$( '#reg-fee-slider' ).slider( "option", "value", amount - workshop_amount );
-		$( '#reg-fee-slider-label').show();
 		$( '.perk' ).each(function() {
 			perk_amount = parseFloat($(this).attr('data-amount'));
 			if (amount >= perk_amount) {
@@ -32,32 +31,35 @@ $(function() {
 		}
 	});
 
+	$('#reg-fee-slider-label').show();
+	$('#reg-fee-workshop-label').show();
+
 	$('#reg-fee').change();
 
-	var bg_student = $('<div class="slider-bg slider-bg-student"><span>Student</span></div>');
+	var bg_student = $('<div class="slider-bg slider-bg-student"></div>');
 	bg_student.css('width', 20.0 / 250.0 * 100 +'%');
 	$('#reg-fee-slider').append(bg_student);
 
-	var bg_normal = $('<div class="slider-bg slider-bg-casual"><span>Hobbyist</span></div>');
+	var bg_normal = $('<div class="slider-bg slider-bg-casual"></div>');
 	bg_normal.css('width', (100 - 20) / 250.0 * 100 +'%');
 	$('#reg-fee-slider').append(bg_normal);
 
-	var bg_normal = $('<div class="slider-bg slider-bg-professional"><span>Professional</span></div>');
+	var bg_normal = $('<div class="slider-bg slider-bg-professional"></div>');
 	bg_normal.css('width', (250 - 100) / 250.0 * 100 +'%');
 	$('#reg-fee-slider').append(bg_normal);
 
 
-	var tick_student = $('<div class="slider-tick"><span>15 €</span></div>');
+	var tick_student = $('<div class="slider-tick"><span>€ 15</span></div>');
 	tick_student.css('width', 15.0 / 250.0 * 100 +'%');
 	tick_student.find('span').mousedown(function (e) {$('#reg-fee').val(15 + workshop_amount); $('#reg-fee').change(); e.stopPropagation();});
 	$('#reg-fee-slider').append(tick_student);
 
-	var tick_casual = $('<div class="slider-tick"><span>40 €</span></div>');
+	var tick_casual = $('<div class="slider-tick"><span>€ 40</span></div>');
 	tick_casual.css('width', (40 - 15.0) / 250.0 * 100 +'%');
 	tick_casual.find('span').mousedown(function (e) {$('#reg-fee').val(40 + workshop_amount); $('#reg-fee').change(); e.stopPropagation();});
 	$('#reg-fee-slider').append(tick_casual);
 
-	var tick_professional = $('<div class="slider-tick"><span>150 €</span></div>');
+	var tick_professional = $('<div class="slider-tick"><span>€ 150</span></div>');
 	tick_professional.css('width', (150 - 40.0) / 250.0 * 100 +'%');
 	tick_professional.find('span').mousedown(function (e) {$('#reg-fee').val(150 + workshop_amount); $('#reg-fee').change(); e.stopPropagation();});
 	$('#reg-fee-slider').append(tick_professional);
@@ -98,6 +100,8 @@ $(function() {
 			$("#reg-fee").change();
 
 		});
+
+		$("#reg-fee-workshop").html('€'+workshop_amount);
 
 	};
 
