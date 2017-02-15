bbe29eb5e66bec9df350eec7eab4cf5925323462
Sam Thursfield <sam@afuera.me.uk>
2017-02-15 13:35:41 +0000

    Allow setting an error to be displayed on the index page
    This will be used when there are internal errors in the OpenID Connect
    auth provider that we need to report. The message will have to be vague
    so we don't risk exposing internal secrets, but a "something failed"
    message is better than a 'login' button that simply does nothing, with
    no indication that there's an error.

--- routes/index.js
+++ routes/index.js
@@ -13,18 +13,20 @@ var utils = require('../utils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const error = req.session['error']; delete req.session['error'];
+
   if(req.session.currentUser)
   {
     if(req.user) {
       req.user.getRegistration({include: [RegistrationPayment]})
       .then(function(reg) {
-        res.render('index/index', { name: req.user.name, registration: reg });
+        res.render('index/index', { name: req.user.name, registration: reg, error: error });
       });
     } else {
-      res.render('index/index', { name: req.session.currentUser });
+      res.render('index/index', { name: req.session.currentUser, error: error });
     }
   } else {
-    res.render('index/index', { });
+    res.render('index/index', { error: error });
   }
 });
 
