c12bbb6ea5af301a6452bad59bc1c1012b6c307d
Sam Thursfield <sam@afuera.me.uk>
2017-02-16 20:06:35 +0000

    Update index to match Allan's design
    This isn't complete, there are some layout issues and I'm not sure the
    we have time to do the "papers" section as per the design, but it's a
    good starting point.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var models = require('../models');
 var User = models.User;
 var Registration = models.Registration;
 var RegistrationPayment = models.RegistrationPayment;
+var RegistrationInfo = models.RegistrationInfo;
 
 var config = require('../configuration');
 
@@ -18,9 +19,11 @@ router.get('/', function(req, res, next) {
   if(req.session.currentUser)
   {
     if(req.user) {
-      req.user.getRegistration({include: [RegistrationPayment]})
+      req.user.getRegistration({include: [RegistrationPayment, { model: RegistrationInfo, include: RegistrationPayment }]})
       .then(function(reg) {
-        res.render('index/index', { name: req.user.name, registration: reg, error: error });
+        res.render('index/index', { name: req.user.name, registration: reg,
+                                    registration_fields: utils.get_reg_fields(null, reg, true),
+                                    error: error });
       });
     } else {
       res.render('index/index', { name: req.session.currentUser, error: error });
